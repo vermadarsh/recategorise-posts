1c6fb96b09daa735982e4a4b22236022ba5de37a
Adarsh Verma <adarsh.srmcem@gmail.com>
2020-01-13 20:31:12 +0530

    added changes as suggested by Devik.

--- recategorise-posts.php
+++ recategorise-posts.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name: Recategorise Posts - Sample WP_CLI
+ * Plugin Name: Recategorise Posts - WP_CLI
  * Author: Adarsh Verma
  * Version: 1.0.0
  * Description: This plugin creates custom wp-cli commands.
@@ -11,21 +11,34 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
+ * Function declared to debug code.
+ */
+if ( ! function_exists( 'debug' ) ) {
+	function debug( $params ) {
+		echo '<pre>';
+		var_dump( $params );
+		echo '</pre>';
+		return;
+	}
+}
+
+/**
  * Add custom command at wp-cli init.
  */
 function lwpcli_initialize() {
-	WP_CLI::add_command( 'categorize_posts', 'wpcliRecategorizePosts' );
+	WP_CLI::add_command( 'categorise_posts', 'wpcli_recategorise_posts' );
 }
+
 add_action( 'cli_init', 'lwpcli_initialize' );
 
 /**
  * Callback class when the custom command shall run.
  */
-if ( ! class_exists( 'wpcliRecategorizePosts' ) ) {
-	class wpcliRecategorizePosts {
+if ( ! class_exists( 'wpcli_recategorise_posts' ) ) {
+	class wpcli_recategorise_posts {
 
 		public function __construct() {
-			$posts = array();
+			
 		}
 
 		/**
@@ -33,39 +46,53 @@ if ( ! class_exists( 'wpcliRecategorizePosts' ) ) {
 		 *
 		 * @return int[]|WP_Post[]
 		 */
-		public function fetch_posts() {
+		public function fetch_posts( $page, $posts_per_page ) {
 			$posts = get_posts(
 				array(
 					'post_type'      => 'post',
-					'posts_per_page' => - 1,
-					'fields'         => 'ids'
+					'posts_per_page' => $posts_per_page,
+					'fields'         => 'ids',
+					'paged'          => $page,
+					'post_status'    => 'any'
 				)
 			);
-			if ( empty( $posts ) ) {
-				WP_CLI::error( esc_html__( 'There isn\'t any post created yet!', 'learn-wpcli' ) );
-			} else {
-				return $posts;
-			}
+
+			return $posts;
 		}
 
 		/**
 		 * Categorize the posts.
 		 */
 		public function categorize_all_posts() {
-			$this->posts = $this->fetch_posts();
-			if ( ! empty( $this->posts ) && is_array( $this->posts ) ) {
-				foreach ( $this->posts as $post_id ) {
-					$categories = get_the_category( $post_id );
-					$has_default_category = $this->check_default_category( $categories );
-					if( ! $has_default_category && empty( $categories ) ) {
-						wp_set_object_terms( $post_id, $this->get_default_category(), 'category' );
+			$total_posts_by_status = wp_count_posts( 'post' );
+			$total_posts           = 0;
+			if ( ! empty( $total_posts_by_status ) && is_object( $total_posts_by_status ) ) {
+				foreach ( $total_posts_by_status as $post_by_status ) {
+					$total_posts += (int) $post_by_status;
+				}
+			}
+			if ( 0 === $total_posts ) {
+				WP_CLI::error( esc_html__( 'There isn\'t any post created yet!', 'learn-wpcli' ) );
+			} else {
+				$posts_per_page = (int) get_option( 'posts_per_page' );
+				$pages          = (int) ( $total_posts / $posts_per_page ) + 1;
+				for ( $i = 1; $i <= $pages; $i ++ ) {
+					$posts = $this->fetch_posts( $i, $posts_per_page );
+					if ( ! empty( $posts ) && is_array( $posts ) ) {
+						foreach ( $posts as $post_id ) {
+							$categories           = get_the_category( $post_id );
+							$has_default_category = $this->check_default_category( $categories );
+							if ( ! $has_default_category && empty( $categories ) ) {
+								wp_set_object_terms( $post_id, $this->get_default_category(), 'category' );
+							} else {
+								wp_remove_object_terms( $post_id, $this->get_default_category(), 'category' );
+							}
+						}
+						WP_CLI::success( esc_html__( 'All the posts are recategorized..!', 'learn-wpcli' ) );
 					} else {
-						wp_remove_object_terms( $post_id, $this->get_default_category(), 'category' );
+						WP_CLI::error( 'There is some error in accessing posts.' );
 					}
 				}
-				WP_CLI::success( esc_html__( 'All the posts are recategorized..!', 'learn-wpcli' ) );
-			} else {
-				WP_CLI::error( 'There is some error in accessing posts.' );
 			}
 		}
 
@@ -78,16 +105,17 @@ if ( ! class_exists( 'wpcliRecategorizePosts' ) ) {
 		 */
 		public function check_default_category( $categories ) {
 
-			if( empty( $categories ) ) {
+			if ( empty( $categories ) ) {
 				return false;
 			} else {
-				if( is_array( $categories ) ) {
-					foreach($categories as $category) {
+				if ( is_array( $categories ) ) {
+					foreach ( $categories as $category ) {
 						$term_id = $category->term_id;
-						if( $term_id === $this->get_default_category() ) {
+						if ( $term_id === $this->get_default_category() ) {
 							return true;
 						}
 					}
+
 					return false;
 				}
 			}
@@ -100,7 +128,7 @@ if ( ! class_exists( 'wpcliRecategorizePosts' ) ) {
 		 * @return int
 		 */
 		public function get_default_category() {
-			return (int) get_option('default_category');
+			return (int) get_option( 'default_category' );
 		}
 
 	}
